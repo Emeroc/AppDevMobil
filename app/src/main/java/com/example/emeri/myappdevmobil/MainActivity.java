0fe70bb930e49c8dcbe997c58af18ed65a77d13f
Emeroc <hoerner.emeric@gmail.com>
2016-11-26 11:17:43 +0100

    Fragement

--- app/src/main/java/com/example/emeri/myappdevmobil/MainActivity.java
+++ app/src/main/java/com/example/emeri/myappdevmobil/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.emeri.myappdevmobil;
 
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        ExampleFragment fragment = new ExampleFragment();
+
+        fragmentTransaction.add(R.id.list1, fragment);
+        fragmentTransaction.commit();
+
     }
 }
